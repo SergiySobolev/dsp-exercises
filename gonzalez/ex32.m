012e22a055688ccff3ff71dac2c5c6573f5ec166
Serhii <Soboliev>
2017-10-01 22:51:15 +0300

    3.2a

---
+++ gonzalez/ex32.m
@@ -0,0 +1,21 @@
+L = 256;
+m = L/2;
+e = 1.4;
+
+r=1:L;
+
+t = 1./(1+(m./r).^e);
+
+I = imread('coins.png');
+set(0,'defaultfigureposition',[150,250,1200,600]);
+figure
+subplot(1,3,1)
+imshow(I);
+
+subplot(1,3,2);
+plot(r,t);
+
+subplot(1,3,3)
+TI = t(I);
+imshow(TI);
+
