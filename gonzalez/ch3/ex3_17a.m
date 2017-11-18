6992063a56e2326015dcf613bb45ec12445e78e5
SergiySobolev <sergiy.sobolev@gmail.com>
2017-11-19 01:08:59 +0200

    Ex 3 17a

--- gonzalez/ch3/ex3_17a.m
+++ gonzalez/ch3/ex3_17a.m
@@ -19,19 +19,15 @@ imshow(AFI);
 function fi = aaimfilter(I, N) 
     [m,n] = size(I);
     N2 = N*N;
-    halfN = floor(N/2);
-    filter = ones(N,N)./N2;
-    BI = zeros(m+N-1, n+N-1);
-    BI(halfN+1:m+halfN, halfN+1:n+halfN) = I;    
+    halfN = floor(N/2); 
     RI = zeros(m,n);
-    RI1 = zeros(m,n);
     
     for i=halfN+1:m-halfN
         BIf = I(i-halfN:i+halfN, 1:N);
         v = sum(BIf(:));
         for j=halfN+1:n-halfN-5
             r=floor(v/N2);
-            RI1(i,j) = r;
+            RI(i,j) = r;
             curF=I(i-halfN:i+halfN, j-halfN:j-halfN);
             nextF=I(i-halfN:i+halfN, j+halfN+1:j+halfN+1);
             dif = minus(double(nextF),double(curF));
@@ -40,15 +36,6 @@ function fi = aaimfilter(I, N)
         end
     end
     
-    fi = mat2gray(RI1);    
+    fi = mat2gray(RI);    
 end
 
-function r = myfilter(I,f,x,y)   
-    N = size(f,1);   
-    [m,n] = size(I);
-    halfN = floor(N/2);
-    BI = zeros(m+N-1, n+N-1);
-    BI(halfN+1:m+halfN, halfN+1:n+halfN) = I;
-    BIf =  BI(x:x+N-1, y:y+N-1);    
-    r = floor(sum(sum(f.*BIf)));
-end
